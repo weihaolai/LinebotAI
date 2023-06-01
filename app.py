1b4005cb9c5dc783977fa300be5a6b0d942f45fd
weihaolai <109146455+weihaolai@users.noreply.github.com>
2023-06-01 17:36:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@
 import os
 import sys
 from argparse import ArgumentParser
+import phonetic as ph
 
 from flask import Flask, request, abort
 from linebot import (
@@ -64,10 +65,11 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-
+        
+        result = ph.read(event.message.text)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text)
+            TextSendMessage(text=result)
         )
 
     return 'OK'
