b25d06b3aa5bd1484f66b940f1e6173682dc2b0a
weihaolai <109146455+weihaolai@users.noreply.github.com>
2023-06-08 17:27:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
 
+# word = input('請輸入一個字')
+def chinese(word):
+    url = f'https://www.twpen.com/{word}.html'
+
+    html = requests.get(url)
+    bs = BeautifulSoup(html.text,'lxml')
+
+    try:
+        dat = bs.find_all('div', class_='site-article-hanziinfo')
+        rad = (dat[0].find_all('p'))[0].text[:-1]
+        num = (dat[0].find_all('p'))[1].text[:-1]
+        chi = (dat[0].find_all('p'))[2].text
+        eng = (dat[0].find_all('p'))[3].text
+
+        dic = [rad,num,chi,eng]
+        for x in dic:
+            print(x)
+
+    except:
+        print('查無資料,請輸入一個字的國字')
